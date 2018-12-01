ea8a9dcc9190701b06a99723bd2ceacff4648dc9
kkrol27 <kyle.krol.27@gmail.com>
2018-11-30 21:15:17 -0500

    imu test

--- Teeter.ino
+++ Teeter.ino
@@ -1,26 +1,44 @@
 
-#include "motor.hpp"
+// #include "motor.hpp"
+#include "LSM6.h"
 
 #define DEBUG
 
+/*
 dev::Motor r_motor(0, 1, 2);
 dev::Motor l_motor(11, 12, 10);
 int speed = -255;
 int ds;
+*/
+
+LSM6 imu;
 
 void setup() {
+
 #ifdef DEBUG
   Serial.begin(9600);
 #endif
+/*
   r_motor.init();
   l_motor.init();
+*/
+  imu.init();
+  imu.enableDefault();
 }
 
 void loop() {
+  imu.read();
+#ifdef DEBUG
+  Serial.println("acc = " + String(imu.a.x) + "," + String(imu.a.y) + "," + String(imu.a.z));
+  Serial.println("gyr = " + String(imu.g.x) + "," + String(imu.g.y) + "," + String(imu.g.z));
+#endif
+/*
   delay(500);
   r_motor.set_speed(speed);
   l_motor.set_speed(speed);
   if (speed >= 255) ds = -5;
   if (speed <= -255) ds = 5;
   speed = speed + ds;
+*/
+  delay(1000);
 }
